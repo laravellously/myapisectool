da2b4cf8753404b6d59ed5f80feafd23920f7344
laravellously <martins.michael71@gmail.com>
2024-08-04 19:13:06 +0100

    update auth.service.ts

--- src/app/core/auth/auth.service.ts
+++ src/app/core/auth/auth.service.ts
@@ -37,7 +37,7 @@ export class AuthService {
 
     getAllResults(): Observable < Result[] > {
       return this._httpClient
-        .get < Result[] > ('/backend/results/get_list?page=1&key=jgujqnaunrvriqutgrawryyaioesfcti')
+        .get < Result[] > ('https://83.229.86.75:5000/results/get_list?page=1&key=jgujqnaunrvriqutgrawryyaioesfcti')
         .pipe(
           tap((response: any) => {
             this._results.next(response);
