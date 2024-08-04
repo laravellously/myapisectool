056a5b22a8bc632f5a0fed47522b350ca59b83e7
laravellously <martins.michael71@gmail.com>
2024-08-04 14:15:28 +0100

    update example.component.ts

--- src/app/modules/admin/example/example.component.ts
+++ src/app/modules/admin/example/example.component.ts
@@ -1,17 +1,24 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, ViewEncapsulation } from '@angular/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
     selector     : 'example',
     standalone   : true,
     templateUrl  : './example.component.html',
     encapsulation: ViewEncapsulation.None,
+    imports: [
+      MatFormFieldModule,
+      MatInputModule,
+      MatIconModule
+    ],
 })
 export class ExampleComponent
 {
     /**
      * Constructor
      */
-    constructor()
-    {
-    }
+    constructor(private _httpClient: HttpClient) {}
 }
